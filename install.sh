eff966ca282ccc5d49c3f88698fedd30ffa53819
kamaboko123 <6112062+kamaboko123@users.noreply.github.com>
2018-04-26 16:19:32 +0900

    fix : install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 AOJCLI=`readlink -f ${SCRIPT_DIR}/aojcli.py`
 
-echo "export alias aojcli='${AOJCLI}'" >> ~/.bashrc
+pip install -r ${SCRIPT_DIR}/requirements.txt
+echo "alias aojcli='${AOJCLI}'" >> ~/.bashrc
 
