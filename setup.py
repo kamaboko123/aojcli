5c05057ade079402456165128ce54c4364e7b15a
kamaboko123 <6112062+kamaboko123@users.noreply.github.com>
2018-09-08 21:08:59 +0900

    make package

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+requires = ["requests", "texttable"]
+
+setup(
+    name='aojcli',
+    version='0.1',
+    description='AOJ submit tool',
+    url='https://github.com/kamaboko123/aojcli',
+    author='kamaboko123',
+    author_email='6112062+kamaboko123@users.noreply.github.com',
+    license='MIT',
+    packages=['aojcli'],
+    install_requires=requires,
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+    ],
+    scripts=['bin/aojcli']
+)
+
